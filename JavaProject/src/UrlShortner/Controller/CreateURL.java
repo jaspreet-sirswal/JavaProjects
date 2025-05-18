f7c7ff803869998be616a64532b4c5466da33bb2
jaspreet-sirswal <jaspreetsirswal2@gmail.com>
2025-05-18 20:02:40 +0530

    added createURL

--- JavaProject/src/UrlShortner/Controller/CreateURL.java
+++ JavaProject/src/UrlShortner/Controller/CreateURL.java
@@ -1,5 +1,11 @@
 package UrlShortner.Controller;
 
-public class CreateURL {
+import UrlShortner.Dao.CreateURLDao;
 
+public class CreateURL {
+    private CreateURLDao createURLDao = new CreateURLDao();
+
+    public String createUrl(String originalUrl, Integer userId, String ExpiryDate) {
+        return createURLDao.CreateAndStoreUrl(originalUrl, userId);
+    }
 }
--- JavaProject/src/UrlShortner/Main.java
+++ JavaProject/src/UrlShortner/Main.java
@@ -1,15 +1,24 @@
 package UrlShortner;
 
+import UrlShortner.Controller.CreateURL;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        CreateURL createURL = new CreateURL();
+        // Creating a url;
+        String shortUrl = createURL.createUrl("https://www.hdfcsec.com/research/stock-market-ideas/investment-ideas?assetId=3&categoryCode=FUND&Sector=&RecoType=&CallType=OPEN&Target_Min=&Target_Max=&horizon_Max=&horizon_Min=&RecoDateRange=%20&BucketID=379&securityId=", 123, "17/05/2025");
+        System.out.printf("shortUrl: " + shortUrl);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+//        for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//            System.out.println("i = " + i);
+//        }
     }
 }
